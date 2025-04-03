c45120b9af5e7e300f71d36ed6bd2a84c03688a3
ArielDW <68842619+ArielDW@users.noreply.github.com>
2025-04-02 20:07:12 -0600

    parses out the edit text from the list

--- extension/content.js
+++ extension/content.js
@@ -3,9 +3,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		const headingsElements = document.querySelectorAll(
 			".mw-heading.mw-heading2"
 		);
-		const headings = Array.from(headingsElements).map(
-			(element) => element.textContent
-		);
+		const headings = Array.from(headingsElements).map((element) =>
+			element.textContent.replace(/\[edit\]$/i, "").trim()
+		); // Remove "[edit]" and trim whitespace
 		chrome.runtime.sendMessage({ headings: headings });
 	}
 });
